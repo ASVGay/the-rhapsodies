19eed2d6cfc74b84220c6ab81e40d134f4d8d416
F.S. Koulen <feryllia.koulen@hva.nl>
2023-04-25 17:19:04 +0200

    Remove unused import and random character

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -83,6 +83,6 @@ export default function App({Component, pageProps}: AppProps) {
         </Head>
         <AuthContextProvider>
             <Component {...pageProps} />
-        </AuthContextProvider> )
+        </AuthContextProvider>
     </>
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,6 @@ import { Inter } from 'next/font/google'
 import {useAuthContext} from "@/context/AuthContext";
 import {useEffect, useState} from "react";
 import {useRouter} from "next/router";
-import signOut from "@/firebase/signout";
 import signOutUser from "@/firebase/signout";
 import MainButton from "@/components/buttons/main-button/MainButton";
 import ErrorPopup from "@/components/popups/error-popup/ErrorPopup";
