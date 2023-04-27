f9007f861f04180206a252f0c683954023dfe5c5
Marcel Hettinga <hettingamarcel@gmail.com>
2023-04-27 14:54:56 +0200

    Remove unnecessary assignment

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   const { user } = useAuthContext();
   const router = useRouter();
   const [showError, setShowError] = useState<boolean>(false);
-  const { signOutUser , isFirstLogin } = useAuthContext();
+  const { signOutUser } = useAuthContext();
 
   useEffect(() => {
     if(!user) {
