b564888336765fdefdf6b44c042b8d03bb379b58
F.S. Koulen <feryllia.koulen@hva.nl>
2023-04-27 11:27:21 +0200

    Create Layout component

---
+++ components/layout/layout.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type LayoutProps = {
+    children: React.ReactNode
+}
+
+export default function Layout({children}: LayoutProps) {
+    return <>
+        {children}
+    </>
+}
