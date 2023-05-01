24f74e3a96dcdae0a73ec80375b805e2bd0533b1
F.S. Koulen <feryllia.koulen@hva.nl>
2023-05-01 22:16:37 +0200

    Configure Firebase for Cypress
    Check for more info on cypress-firebase:
    https://github.com/prescottprue/cypress-firebase#cycallfirestore

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,25 @@
-import { defineConfig } from "cypress";
+require('dotenv').config()
+import admin from 'firebase-admin';
+import {defineConfig} from 'cypress';
+import {plugin as cypressFirebasePlugin} from 'cypress-firebase';
 
 export default defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    e2e: {
+        baseUrl: 'http://localhost:3000',
+        // NOTE: Add "supportFile" setting if separate location is used
+        setupNodeEvents(on, config) {
+            config.env = {
+                ...process.env,
+                ...config.env
+            }
+            // e2e testing node events setup code
+            return cypressFirebasePlugin(on, config, admin, {
+                // Here is where you can pass special options.
+                // If you have not set the GCLOUD_PROJECT environment variable, give the projectId here, like so:
+                projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+                // if your databaseURL is not just your projectId plus ".firebaseio.com", then you _must_ give it here, like so:
+                //    databaseURL: 'some-project-default-rtdb.europe-west1.firebasedatabase.app',
+            });
+        },
     },
-  },
 });
