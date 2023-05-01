dd40ef389c34117bd438f017ac8ad53906941fea
F.S. Koulen <feryllia.koulen@hva.nl>
2023-05-01 23:01:10 +0200

    Collapse empty tag and add break line

--- components/layout/navigation/top-navigation/TopNavigation.tsx
+++ components/layout/navigation/top-navigation/TopNavigation.tsx
@@ -8,31 +8,33 @@ import {NextRouter, useRouter} from "next/router";
 export default function TopNavigation() {
     const router: NextRouter = useRouter();
     const settingsItem: NavItem | undefined = NavigationItems.find(item => item.text == "Settings");
-    return (<div className={"hidden lg:flex bg-white h-24 justify-between items-center px-2"}>
 
-        <Link href={"/"}>
-            <Image src={"/images/logo.png"} alt={"Logo of The Rhapsodies"} height={96} width={96}/>
-        </Link>
+    return (
+        <div className={"hidden lg:flex bg-white h-24 justify-between items-center px-2"}>
+            <Link href={"/"}>
+                <Image src={"/images/logo.png"} alt={"Logo of The Rhapsodies"} height={96} width={96}/>
+            </Link>
 
-        <div className={"flex"}>
-            {NavigationItems.map((item, index): React.ReactNode => {
-                if (item.text == "Settings") return
-                return <Link key={item.path} href={item.path}
-                             className={`text-xl px-4 ${isActive(item.path, router) ? "text-moon-400" : "text-zinc-300"}`}>
-                    <div className={"relative group "}>
-                        <span>{item.text}</span>
-                        <span
-                            className="absolute -bottom-1 left-0 w-0 transition-all h-0.5 bg-moon-300 group-hover:w-full"></span>
-                    </div>
+            <div className={"flex"}>
+                {NavigationItems.map((item, index): React.ReactNode => {
+                    if (item.text == "Settings") return
+                    return <Link key={item.path} href={item.path}
+                                 className={`text-xl px-4 ${isActive(item.path, router) ? "text-moon-400" : "text-zinc-300"}`}>
+                        <div className={"relative group "}>
+                            <span>{item.text}</span>
+                            <span
+                                className="absolute -bottom-1 left-0 w-0 transition-all h-0.5 bg-moon-300 group-hover:w-full"/>
+                        </div>
+                    </Link>
+                })}
+            </div>
+
+            {settingsItem == undefined ? <span className={"w-24"}/> :
+                <Link href={settingsItem.path} key={settingsItem.path} className={"w-24"}>
+                    <Cog8ToothIcon
+                        className={`h-9 float-right hover:text-moon-200 hover:rotate-180 transition duration-500 ease-in-out ${isActive(settingsItem.path, router) ? "text-moon-400" : "text-zinc-300"}`}/>
                 </Link>
-            })}
+            }
         </div>
-
-        {settingsItem == undefined ? <span className={"w-24"}/> :
-            <Link href={settingsItem.path} key={settingsItem.path} className={"w-24"}>
-                <Cog8ToothIcon
-                    className={`h-9 float-right hover:text-moon-200 hover:rotate-180 transition duration-500 ease-in-out ${isActive(settingsItem.path, router) ? "text-moon-400" : "text-zinc-300"}`}/>
-            </Link>}
-
-    </div>)
+    )
 }
