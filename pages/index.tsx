67cc13c05408edc00fe9224676463d02d42889d3
Marcel Hettinga <hettingamarcel@gmail.com>
2023-05-01 12:18:33 +0200

    Removed old authentication code

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,23 +5,10 @@ import MainButton from "@/components/buttons/main-button/MainButton";
 import ErrorPopup from "@/components/popups/error-popup/ErrorPopup";
 import WithProtectedRoute from "@/components/protected-route/ProtectedRoute";
 
-
 function Home() {
   const router = useRouter();
   const [showError, setShowError] = useState<boolean>(false);
   const { signOutUser , user, isFirstLogin  } = useAuthContext();
-
-  // useEffect(() => {
-  //   if(!user) {
-  //     router.push("./signin")
-  //   }
-  // },)
-  //
-  // useEffect(() => {
-  //   if(isFirstLogin) {
-  //     router.push("./change-password")
-  //   }
-  // },[isFirstLogin, router])
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
