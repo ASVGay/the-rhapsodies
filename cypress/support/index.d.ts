dceaf7d09480c2d44c9e2a3b7a8dd9dcba27b7ef
F.S. Koulen <feryllia.koulen@hva.nl>
2023-05-07 14:18:13 +0200

    Create custom command for selecting data-cy

---
+++ cypress/support/index.d.ts
@@ -0,0 +1,13 @@
+declare global {
+    namespace Cypress {
+        interface Chainable {
+            /**
+             * Custom command to select DOM element by data-cy attribute.
+             * @example cy.data('greeting')
+             */
+            data(value: string): Chainable<JQuery<HTMLElement>>
+        }
+    }
+}
+
+export {}
