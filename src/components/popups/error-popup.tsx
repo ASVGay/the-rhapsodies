f7ad582491b11611cb275edbf0390596f11d6b9c
Marcel Hettinga <hettingamarcel@gmail.com>
2023-05-09 14:43:29 +0200

    Make datacy optional

--- src/components/popups/error-popup.tsx
+++ src/components/popups/error-popup.tsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 interface ErrorPopupProps {
   text: string
   closePopup: () => void
-  dataCy: string
+  dataCy?: string
 }
 const ErrorPopup = (props: ErrorPopupProps) => {
   return (
