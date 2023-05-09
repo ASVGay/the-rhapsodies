d7df293a19414df0dc1482bd634527fcd74731ab
F.S. Koulen <feryllia.koulen@hva.nl>
2023-05-09 15:17:32 +0200

    Show instruction if notifs are not supported

--- src/components/settings/notifications/notification-settings.tsx
+++ src/components/settings/notifications/notification-settings.tsx
@@ -2,11 +2,17 @@ import React from "react"
 import SettingsWrapper from "@/components/settings/settings-wrapper"
 import { BellIcon } from "@heroicons/react/24/outline"
 import EnableNotificationsToggle from "@/components/settings/notifications/enable-notifications-toggle"
+import { notificationsAreSupported } from "@/helpers/pwa.helper"
 
 const NotificationSettings = () => {
   return (
     <SettingsWrapper category={"Notifications"} icon={BellIcon}>
       <EnableNotificationsToggle />
+      {!notificationsAreSupported() && (
+        <small className={"block italic text-zinc-300"}>
+          You need to add this application to your home screen if you want to enable notifications.
+        </small>
+      )}
     </SettingsWrapper>
   )
 }
