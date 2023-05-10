ef740ba018ef79a01254632b44cc3e63fe677c38
Marcel Hettinga <hettingamarcel@gmail.com>
2023-05-10 16:47:16 +0200

    Fix formatting

--- src/pages/change-password/index.tsx
+++ src/pages/change-password/index.tsx
@@ -1,124 +1,127 @@
-import React, {useState} from "react"
+import React, { useState } from "react"
 import SignInTextField from "@/components/text-fields/sign-in-text-field"
 import MainButton from "@/components/buttons/main-button"
-import {useAuthContext} from "@/context/auth-context"
+import { useAuthContext } from "@/context/auth-context"
 import ErrorPopup from "@/components/popups/error-popup"
-import {mapAuthErrorCodeToErrorMessage} from "@/helpers/sign-in.helper"
-import {FirebaseError} from "@firebase/util"
+import { mapAuthErrorCodeToErrorMessage } from "@/helpers/sign-in.helper"
+import { FirebaseError } from "@firebase/util"
 import WithProtectedRoute from "@/components/protected-route/protected-route"
-import {changePassword, setAdditionalUserData, signOutUser, updateUserName} from "@/services/authentication.service"
-import {ErrorCodes} from "@/constants/error-codes"
+import {
+  changePassword,
+  setAdditionalUserData,
+  signOutUser,
+  updateUserName,
+} from "@/services/authentication.service"
+import { ErrorCodes } from "@/constants/error-codes"
 
 const Index = () => {
-    const [password, setPassword] = useState<string>("")
-    const [userName, setUserName] = useState<string>("")
-    const [confirmPassword, setConfirmPassword] = useState<string>()
-    const [errorText, setErrorText] = useState<string>("")
-    const [showErrorText, setShowErrorText] = useState<boolean>(false)
-    const {user} = useAuthContext()
+  const [password, setPassword] = useState<string>("")
+  const [userName, setUserName] = useState<string>("")
+  const [confirmPassword, setConfirmPassword] = useState<string>()
+  const [errorText, setErrorText] = useState<string>("")
+  const [showErrorText, setShowErrorText] = useState<boolean>(false)
+  const { user } = useAuthContext()
 
-    const setError = (errorText: string) => {
-        setErrorText(errorText)
-        setShowErrorText(true);
+  const setError = (errorText: string) => {
+    setErrorText(errorText)
+    setShowErrorText(true)
+  }
+
+  const handleFireBaseError = (err: FirebaseError) => {
+    setErrorText(mapAuthErrorCodeToErrorMessage(err.code))
+    if (err.code === ErrorCodes.REQUIRE_RECENT_LOGIN) {
+      setTimeout(async () => {
+        await signOutUser()
+      }, 5000)
+    }
+    setShowErrorText(true)
+  }
+  const submitNewPassword = () => {
+    if (!password) {
+      setError("Please enter a password.")
+      return
     }
 
-    const handleFireBaseError = (err: FirebaseError) => {
-        setErrorText(mapAuthErrorCodeToErrorMessage(err.code))
-        if (err.code === ErrorCodes.REQUIRE_RECENT_LOGIN) {
-            setTimeout(async () => {
-                await signOutUser()
-            }, 5000)
-        }
-        setShowErrorText(true)
-    }
-    const submitNewPassword = () => {
-        if (!password) {
-            setError("Please enter a password.")
-            return
-        }
-
-        if (!user) {
-            return
-        }
-
-        if (password !== confirmPassword) {
-            setError("Fill in equal passwords.")
-            return
-        }
-
-        if (userName.length < 1) {
-            setError("Please fill in a username.")
-        }
-
-        changePassword(password, user.user)
-            .then(() => {
-                updateUserName(userName, user.user)
-                    .catch((error) => {
-                        const err = error as FirebaseError
-                        handleFireBaseError(err);
-                    })
-            })
-            .catch((error) => {
-                const err = error as FirebaseError
-                handleFireBaseError(err)
-            })
-
-
+    if (!user) {
+      return
     }
 
-    return (
-        <div>
-            <div className={"flex h-screen w-screen items-center justify-center bg-moon-50"}>
-                <div
-                    className={
-                        "flex h-fit w-80 flex-col justify-between gap-6 rounded-lg bg-zinc-50 p-4 bg-blend-hard-light"
-                    }
-                >
-                    {user?.additionalUserData.isFirstLogin && (
-                        <div className={"flex w-full justify-center flex-col gap-6"}>
+    if (password !== confirmPassword) {
+      setError("Fill in equal passwords.")
+      return
+    }
+
+    if (userName.length < 1) {
+      setError("Please fill in a username.")
+    }
+
+    changePassword(password, user.user)
+      .then(() => {
+        updateUserName(userName, user.user).catch((error) => {
+          const err = error as FirebaseError
+          handleFireBaseError(err)
+        })
+      })
+      .catch((error) => {
+        const err = error as FirebaseError
+        handleFireBaseError(err)
+      })
+  }
+
+  return (
+    <div>
+      <div className={"flex h-screen w-screen items-center justify-center bg-moon-50"}>
+        <div
+          className={
+            "flex h-fit w-80 flex-col justify-between gap-6 rounded-lg bg-zinc-50 p-4 bg-blend-hard-light"
+          }
+        >
+          {user?.additionalUserData.isFirstLogin && (
+            <div className={"flex w-full flex-col justify-center gap-6"}>
               <span className={"w-fit font-semibold leading-8 text-black"}>
-                In order to access the application you need to change your password and set your username.
+                In order to access the application you need to change your password and set your
+                username.
               </span>
 
-                            <SignInTextField
-                                dataCy={"set-name-textfield"}
-                                placeholder={"Username"}
-                                type={"input"}
-                                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setUserName(e.target.value)}
-                            />
-                        </div>
-                    )}
-
-                    <div className="w-full">
-                        <SignInTextField
-                            dataCy={"change-password-textfield"}
-                            placeholder={"Password"}
-                            type={"password"}
-                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
-                        />
-                    </div>
-                    <div>
-                        <SignInTextField
-                            dataCy={"change-password-confirm-textfield"}
-                            placeholder={"Confirm Password"}
-                            type={"password"}
-                            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                                setConfirmPassword(e.target.value)
-                            }
-                        />
-                    </div>
-                    {showErrorText && (
-                        <ErrorPopup
-                            dataCy={"error-popup-change-password"}
-                            text={errorText}
-                            closePopup={() => setShowErrorText(false)}
-                        />
-                    )}
-                    <MainButton dataCy={"submit-password-btn"} onClick={submitNewPassword} text={"Submit"}/>
-                </div>
+              <SignInTextField
+                dataCy={"set-name-textfield"}
+                placeholder={"Username"}
+                type={"input"}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setUserName(e.target.value)}
+              />
             </div>
+          )}
+
+          <div className="w-full">
+            <SignInTextField
+              dataCy={"change-password-textfield"}
+              placeholder={"Password"}
+              type={"password"}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
+            />
+          </div>
+          <div>
+            <SignInTextField
+              dataCy={"change-password-confirm-textfield"}
+              placeholder={"Confirm Password"}
+              type={"password"}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setConfirmPassword(e.target.value)
+              }
+            />
+          </div>
+          {showErrorText && (
+            <ErrorPopup
+              dataCy={"error-popup-change-password"}
+              text={errorText}
+              closePopup={() => setShowErrorText(false)}
+            />
+          )}
+          <MainButton dataCy={"submit-password-btn"} onClick={submitNewPassword} text={"Submit"} />
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default WithProtectedRoute(Index)
