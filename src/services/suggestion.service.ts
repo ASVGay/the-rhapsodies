286cda53abe4a5cd75a59244039d937fe1df92dd
F.S. Koulen <feryllia.koulen@hva.nl>
2023-05-12 20:34:36 +0200

    Remove firebase from suggestion service

--- src/services/suggestion.service.ts
+++ src/services/suggestion.service.ts
@@ -1,32 +1,37 @@
 import { collection, getDocs } from "firebase/firestore"
-import { db } from "@/firebase/config"
 import { ISuggestion } from "@/interfaces/suggestion"
 import { doc, getDoc, updateDoc } from "@firebase/firestore"
 
 export const getSuggestions = async (): Promise<ISuggestion[]> => {
-  const querySnapshot = await getDocs(collection(db, "suggestions"))
-
-  const suggestions: ISuggestion[] = []
-  querySnapshot.forEach((doc) => {
-    const suggestion = doc.data() as ISuggestion
-    suggestion.id = doc.id
-    suggestions.push(suggestion)
-  })
-
-  return suggestions
+  // TODO Enter logic to get suggestions
+  // const querySnapshot = await getDocs(collection(db, "suggestions"))
+  //
+  // const suggestions: ISuggestion[] = []
+  // querySnapshot.forEach((doc) => {
+  //   const suggestion = doc.data() as ISuggestion
+  //   suggestion.id = doc.id
+  //   suggestions.push(suggestion)
+  // })
+  //
+  // return suggestions
+  return Promise.reject("Some Error Message")
 }
 
 export const getSuggestion = async (id: string): Promise<ISuggestion> => {
-  const querySnapshot = await getDoc(doc(db, "suggestions", id))
-  return {
-    ...querySnapshot.data(),
-    id: querySnapshot.id,
-    date: querySnapshot.data()?.date.toDate(),
-  } as ISuggestion
+  // TODO Enter logic to get a singular suggestion
+  // const querySnapshot = await getDoc(doc(db, "suggestions", id))
+  // return {
+  //   ...querySnapshot.data(),
+  //   id: querySnapshot.id,
+  //   date: querySnapshot.data()?.date.toDate(),
+  // } as ISuggestion
+  return Promise.reject("Some Error Message")
 }
 
 export const updateSuggestion = (suggestion: ISuggestion) => {
-  return updateDoc(doc(db, "suggestions", suggestion.id), {
-    roles: suggestion.roles,
-  })
+  // TODO Enter logic to update a suggestion (or just the division)
+  // return updateDoc(doc(db, "suggestions", suggestion.id), {
+  //   roles: suggestion.roles,
+  // })
+  return Promise.reject("Some Error Message")
 }
