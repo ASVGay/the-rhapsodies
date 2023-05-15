3633b0aa77692c96b5e543c675eb6299c959c916
Kevin Breurken <kevinbreurken@gmail.com>
2023-05-16 00:32:06 +0200

    Add retry to Cypress tests with env variable

--- cypress.config.ts
+++ cypress.config.ts
@@ -9,6 +9,9 @@ export default defineConfig({
   e2e: {
     defaultCommandTimeout: 1000 * parseInt(process.env.CYPRESS_TIMEOUT_SECONDS || "12"),
     baseUrl: process.env.CYPRESS_BASE_URL,
+    retries: {
+      runMode: parseInt(process.env.CYPRESS_RETRY_MODE_RUN || "0"),
+    },
     // NOTE: Add "supportFile" setting if separate location is used
     setupNodeEvents(on, config) {
       on("file:preprocessor", createBundler())
