b8cda2098009074922bd9917738776499f6ad920
F.S. Koulen <feryllia.koulen@hva.nl>
2023-05-15 09:51:11 +0200

    Createa database types for easier type assurance

---
+++ src/types/database-types.ts
@@ -0,0 +1,27 @@
+import { Database } from "@/types/database"
+
+export type DisplayName = { display_name: string }
+
+export type Instrument = Database["public"]["Tables"]["instrument"]["Row"]
+
+export type DivisionDatabaseOperation = Database["public"]["Tables"]["division"]["Insert"]
+
+export type Division = Database["public"]["Tables"]["division"]["Row"] & {
+  musician: {
+    display_name: string
+    id: string
+  }
+}
+
+export type SuggestionInstrument = Database["public"]["Tables"]["suggestion_instrument"]["Row"] & {
+  instrument: Instrument
+  division: Division[]
+}
+
+export type Suggestion = Database["public"]["Tables"]["suggestion"]["Row"] & {
+  suggestion_instruments: SuggestionInstrument[]
+  author: string | DisplayName
+}
+
+// Give it an alias since Suggestion is already defined in [suggestion].tsx
+export type SuggestionType = Suggestion
