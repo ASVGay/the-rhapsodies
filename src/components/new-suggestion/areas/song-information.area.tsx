314320a20dc7e359bb20c2bffd25d4f7adcb7ad5
F.S. Koulen <feryllia.koulen@hva.nl>
2023-05-21 18:57:12 +0200

    Store song information in Redux store

--- src/components/new-suggestion/areas/song-information.area.tsx
+++ src/components/new-suggestion/areas/song-information.area.tsx
@@ -1,19 +1,61 @@
 import React from "react"
 import { DocumentTextIcon, LinkIcon, UserIcon } from "@heroicons/react/24/outline"
+import { useForm } from "react-hook-form"
+import { useDispatch, useSelector } from "react-redux"
+import { AppDispatch, AppState } from "@/redux/store"
+import { setActiveArea, updateNewSuggestion } from "@/redux/slices/new-suggestion.slice"
+import { Area } from "@/constants/area"
+
+interface Inputs {
+  artist: string
+  link: string
+  motivation: string
+  title: string
+}
 
 const SongInformationArea = () => {
+  const newSuggestion = useSelector((state: AppState) => state.newSuggestion.suggestion)
+  const dispatch: AppDispatch = useDispatch()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>({
+    defaultValues: { ...newSuggestion, artist: newSuggestion.artist.join(",") } as Inputs,
+  })
+
+  const onSubmit = ({ title, artist, link, motivation }: Inputs) => {
+    dispatch(
+      updateNewSuggestion({
+        ...newSuggestion,
+        title,
+        artist: [artist],
+        link,
+        motivation,
+      })
+    )
+    dispatch(setActiveArea(Area.Instruments))
+  }
   return (
     <div data-cy="area-song-information">
       <h2 className={"area-header"}>Song information</h2>
 
-      <form className={"mx-auto sm:w-4/5 lg:w-full"}>
+      <form className={"mx-auto sm:w-4/5 lg:w-full"} onSubmit={handleSubmit(onSubmit)}>
         <div>
           <label htmlFor="title" className="sr-only">
             Title
           </label>
 
           <div className="input">
-            <input type="text" placeholder="Title" />
+            <input
+              type="text"
+              placeholder="Title"
+              {...register("title", {
+                validate: (value) => {
+                  return !!value.trim()
+                },
+              })}
+            />
             <span>
               <DocumentTextIcon />
             </span>
@@ -22,11 +64,19 @@ const SongInformationArea = () => {
 
         <div>
           <label htmlFor="artist" className="sr-only">
-            Artist
+            Artist(s)
           </label>
 
           <div className="input">
-            <input type="text" placeholder="Artist" />
+            <input
+              type="text"
+              placeholder="Artist"
+              {...register("artist", {
+                validate: (value: string) => {
+                  return !!value.trim()
+                },
+              })}
+            />
             <span>
               <UserIcon />
             </span>
@@ -35,11 +85,11 @@ const SongInformationArea = () => {
 
         <div>
           <label htmlFor="link" className="sr-only">
-            Link to song
+            Link to the song (optional)
           </label>
 
           <div className="input">
-            <input type="url" placeholder="Link to song" />
+            <input type="url" placeholder="Link to the song (optional)" {...register("link")} />
             <span>
               <LinkIcon />
             </span>
@@ -56,6 +106,11 @@ const SongInformationArea = () => {
               className="mb-6 w-full rounded-lg border border-gray-200 p-3 shadow-sm"
               rows={4}
               placeholder="Explain why you would like to play this song with The Rhapsodies."
+              {...register("motivation", {
+                validate: (value) => {
+                  return !!value.trim()
+                },
+              })}
             />
           </div>
         </div>
