ca9cf958f0cd8d7c3d3938a2d7f19d6002d59f6f
Kevin Breurken <kevinbreurken@gmail.com>
2023-05-22 00:19:30 +0200

    Add fetching of instruments in new suggestion page

--- src/pages/suggestions/new/index.tsx
+++ src/pages/suggestions/new/index.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { XMarkIcon } from "@heroicons/react/24/outline"
 import ProgressBar from "@/components/new-suggestion/progress-bar/progress-bar"
 import { useRouter } from "next/router"
@@ -8,10 +8,44 @@ import { Area } from "@/constants/area"
 import { useSelector } from "react-redux"
 import { AppState } from "@/redux/store"
 import InstrumentsArea from "@/components/new-suggestion/areas/instruments/instruments.area"
+import { Database } from "@/types/database"
+import { Instrument } from "@/types/database-types"
+import { useSupabaseClient } from "@supabase/auth-helpers-react"
+import { getInstruments } from "@/services/instruments.service"
+import Spinner from "@/components/utils/spinner"
+import ErrorPopup from "@/components/popups/error-popup"
 
 const NewSuggestion = () => {
   const router = useRouter()
   const activeArea = useSelector((state: AppState) => state.newSuggestion.activeArea)
+
+  const supabaseClient = useSupabaseClient<Database>()
+  const [instruments, setInstruments] = useState<Instrument[]>([])
+  const [showSpinner, setShowSpinner] = useState<boolean>(false)
+  const [showLoadingError, setShowLoadingError] = useState<boolean>(false)
+
+  useEffect(() => {
+    setShowSpinner(true)
+    getInstruments(supabaseClient)
+      .then((response) => {
+        if (response.error || response.data?.length! > 0) {
+          setShowLoadingError(true)
+          return
+        } else {
+          setInstruments(response.data as Instrument[])
+        }
+
+        //   response.data?.length! > 0
+        //     ? setSuggestions(response.data as Suggestion[])
+        //     : setNoSuggestionsMade(true)
+      })
+      .catch(() => {
+        setShowLoadingError(true)
+      })
+      .finally(() => {
+        setShowSpinner(false)
+      })
+  }, [supabaseClient])
 
   return (
     <div className={"page-wrapper"}>
@@ -23,13 +57,25 @@ const NewSuggestion = () => {
           onClick={() => router.push("/suggestions")}
         />
       </div>
+      {showSpinner && (
+        <div className={"h-[75vh] text-center"} data-cy="suggestions-spinner">
+          <Spinner size={10} />
+        </div>
+      )}
 
-      <div className={"mx-auto text-center lg:w-2/4"}>
-        <ProgressBar />
-        {activeArea == Area.SongInformation && <SongInformationArea />}
-        {activeArea == Area.Instruments && <InstrumentsArea />}
-        {activeArea == Area.Review && <ReviewArea />}
-      </div>
+      {showLoadingError && (
+        <div className={"mt-6"} data-cy="failed-fetching-suggestions">
+          <ErrorPopup text={"Failed to load suggestions."} closePopup={() => {}} />
+        </div>
+      )}
+      {!showLoadingError && (
+        <div className={"mx-auto text-center lg:w-2/4"}>
+          <ProgressBar />
+          {activeArea == Area.SongInformation && <SongInformationArea />}
+          {activeArea == Area.Instruments && <InstrumentsArea />}
+          {activeArea == Area.Review && <ReviewArea />}
+        </div>
+      )}
     </div>
   )
 }
---
+++ src/services/instruments.service.ts
@@ -0,0 +1,9 @@
+import { SupabaseClient } from "@supabase/supabase-js"
+import { Database } from "@/types/database"
+
+export const getInstruments = async (supabase: SupabaseClient<Database>) => {
+  return supabase
+    .from("instrument")
+    .select("id, instrument_name, image_source")
+    .order("instrument_name", { ascending: true })
+}
