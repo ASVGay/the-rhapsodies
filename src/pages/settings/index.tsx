a6bbdc58ade89611af82dcb90ad129af2ec3f9fe
F.S. Koulen <feryllia.koulen@hva.nl>
2023-05-22 18:53:20 +0200

    Add link to Icons8

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -5,6 +5,12 @@ export default function Settings() {
     <div className={"page-wrapper lg:w-3/5"}>
       <h1 className={"page-header lg:text-center"}>Settings</h1>
       <NotificationSettings />
+      <p className={"bottom-0 pt-6 text-center italic text-zinc-300"}>
+        Icons by{" "}
+        <a href={"https://icons8.com"} className={"underline"}>
+          Icons8
+        </a>
+      </p>
     </div>
   )
 }
