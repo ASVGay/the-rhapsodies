a743897f68f8ccb25aabd6e504de00900f789b12
F.S. Koulen <feryllia.koulen@hva.nl>
2023-05-25 02:17:06 +0200

    Create UI for check your email

--- cypress/e2e/forgot-password.cy.ts
+++ cypress/e2e/forgot-password.cy.ts
@@ -2,6 +2,17 @@ describe("the forgot password page", () => {
   const inputEmail = "input-email"
   const buttonSubmit = "button-submit-email"
   const error = "input-email-error"
+  const checkYourMail = "check-your-mail"
+  const requestLink = "request-link"
+  const tryAgain = "try-again"
+  const backToSignIn = "back-to-sign-in"
+  const testEmail = "test@email"
+  const emailValue = "email-value"
+  const successMessage = "An email has been sent. Check your spam folder if you cannot find it."
+  const errorLink = {
+    code: 429,
+    msg: "For security purposes, you can only request this once every 60 seconds",
+  }
   beforeEach(() => {
     cy.visit("/forgot-password")
   })
@@ -10,8 +21,15 @@ describe("the forgot password page", () => {
     cy.login()
   })
 
+  it("should render request password area", () => {
+    cy.data(checkYourMail).should("not.exist")
+    cy.data(requestLink).should("be.visible")
+    cy.data(inputEmail).should("be.visible")
+    cy.data(buttonSubmit).should("be.visible")
+  })
+
   it("should go back to login on click", () => {
-    cy.data("back-to-sign-in").click()
+    cy.data(backToSignIn).click()
     cy.location("pathname").should("equal", "/sign-in")
   })
 
@@ -22,17 +40,65 @@ describe("the forgot password page", () => {
     cy.data(inputEmail).should("have.css", "outline-color", "rgb(248, 113, 113)")
   })
 
-  it.only("should show error toast on error of request", () => {
-    cy.data(inputEmail).type("test@email")
-    const error = {
-      code: 429,
-      msg: "For security purposes, you can only request this once every 60 seconds",
-    }
-    cy.intercept("POST", "/auth/v1/recover", {
+  it("should show error toast on error of request", () => {
+    cy.data(inputEmail).type(testEmail)
+    cy.intercept("POST", "/auth/v1/recover*", {
       statusCode: 429,
-      body: { ...error },
+      body: { ...errorLink },
     })
     cy.data(buttonSubmit).click()
-    cy.get(".Toastify").get("#1").get(".Toastify__toast-body").should("have.text", error.msg)
+    cy.get(".Toastify").get("#1").get(".Toastify__toast-body").should("have.text", errorLink.msg)
+  })
+
+  context("on submit", () => {
+    beforeEach("submit email", () => {
+      cy.data(inputEmail).type(testEmail)
+      cy.data(buttonSubmit).click()
+    })
+
+    it("should render check your email area", () => {
+      cy.data(requestLink).should("not.exist")
+      cy.data(tryAgain).should("be.visible")
+      cy.data(backToSignIn).should("be.visible")
+      cy.data(emailValue).should("be.visible")
+    })
+
+    it("should display submitted email", () => {
+      cy.data(emailValue).should("contain.text", testEmail)
+    })
+
+    it("should go back to login on click", () => {
+      cy.data(backToSignIn).click()
+      cy.location("pathname").should("equal", "/sign-in")
+    })
+
+    it("should re-request link with correct email on try again", () => {
+      cy.intercept("POST", "/auth/v1/recover*", {
+        statusCode: 200,
+        body: {},
+      }).as("link-request")
+      cy.data(tryAgain).click()
+      cy.wait("@link-request").its("request.body.email").should("include", testEmail)
+    })
+
+    it("should show toast on succes of try again", () => {
+      cy.intercept("POST", "/auth/v1/recover*", {
+        statusCode: 200,
+        body: {},
+      }).as("link-request")
+      cy.data(tryAgain).click()
+      cy.wait("@link-request")
+      cy.get(".Toastify").get("#1").get(".Toastify__toast-body").should("have.text", successMessage)
+    })
+
+    it("should show toast on error of try again", () => {
+      cy.intercept("POST", "/auth/v1/recover*", {
+        statusCode: 429,
+        body: { ...errorLink },
+      }).as("link-request")
+      cy.data(tryAgain).click()
+      cy.wait("@link-request")
+      cy.get(".Toastify").get("#1").get(".Toastify__toast-body").should("have.text", errorLink.msg)
+    })
   })
 })
--- src/pages/forgot-password/index.tsx
+++ src/pages/forgot-password/index.tsx
@@ -15,70 +15,125 @@ const Index = () => {
   const supabase = useSupabaseClient()
   const [emailIsSent, setEmailIsSent] = useState<boolean>(false)
   const resetPasswordForEmail = async ({ email }: Input) => {
-    const { error } = await supabase.auth.resetPasswordForEmail(email)
-    if (error) toast.error(error.message)
+    const { error } = await supabase.auth.resetPasswordForEmail(email, {
+      redirectTo: `${window.location.origin}/forget-password/reset`,
+    })
+    if (error) {
+      toast.error(error.message)
+      return false
+    } else {
+      setEmailIsSent(true)
+      return true
+    }
   }
 
   const {
+    watch,
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<Input>()
 
+  const emailValue = watch("email")
+
+  const resendEmail = async () => {
+    const isSuccess = await resetPasswordForEmail({ email: emailValue })
+    if (isSuccess)
+      toast.success("An email has been sent. Check your spam folder if you cannot find it.")
+  }
+
   return (
     <div className={"full-bg-moon-50"}>
-      <div className={"auth-container text-center"}>
-        <Image
-          src={"/images/undraw-forgot-password.svg"}
-          alt={"Person looking confused at password field"}
-          width={0}
-          height={0}
-          style={{ width: "100%" }}
-        />
-        <span className={"text-xl font-semibold"}>Forgotten your password?</span>
-        <p className={"text-zinc-400"}>
-          Enter your email and we&#39;ll send you a link to reset your password.
-        </p>
-        <form onSubmit={handleSubmit(resetPasswordForEmail)}>
-          <div className={"input-container"}>
-            <label htmlFor="email" className="sr-only">
-              Enter your email
-            </label>
-            {errors.email && (
-              <ErrorMessage
-                dataCy={"input-email-error"}
-                message={"Please enter your email address"}
-              />
-            )}
-            <div className="input">
-              <input
-                className={`bg-zinc-50 !p-2.5 ${errors.email && "error"}`}
-                data-cy={"input-email"}
-                type="email"
-                placeholder="Email"
-                {...register("email", { required: true })}
-              />
-              <span>
-                <EnvelopeIcon />
-              </span>
+      {emailIsSent && (
+        <div className={"auth-container text-center"} data-cy={"check-your-mail"}>
+          <Image
+            src={"/images/undraw-mail-sent.svg"}
+            alt={"Open envelope with checkmark"}
+            className={"animate-fade-down animate-once"}
+            width={0}
+            height={0}
+            style={{ width: "100%" }}
+          />
+          <span className={"text-xl font-semibold"}>Check your email</span>
+          <span className={"text-sm"}>
+            Email sent to: <b data-cy={"email-value"}>{emailValue}</b>
+          </span>
+          <p className={"text-zinc-700"}>
+            If an user with this email exists, you will receive an email with a link to reset your
+            password.
+          </p>
+          <div className={"flex justify-between"}>
+            <div
+              className={"text-center text-sm text-moon-500"}
+              data-cy={"try-again"}
+              onClick={() => resendEmail()}
+            >
+              Try again
             </div>
+            <Link
+              href={"/sign-in"}
+              className={"text-center text-sm text-gray-400"}
+              data-cy={"back-to-sign-in"}
+            >
+              Back to login
+            </Link>
           </div>
-          <button
-            data-cy={"button-submit-email"}
-            type={"submit"}
-            className={"btn w-full rounded-lg p-2.5"}
+        </div>
+      )}
+      {!emailIsSent && (
+        <div className={"auth-container text-center"} data-cy={"request-link"}>
+          <Image
+            src={"/images/undraw-forgot-password.svg"}
+            alt={"Person looking confused at password field"}
+            width={0}
+            height={0}
+            style={{ width: "100%" }}
+          />
+          <span className={"text-xl font-semibold"}>Forgotten your password?</span>
+          <p className={"text-zinc-400"}>
+            Enter your email and we{`'`}ll send you a link to reset your password.
+          </p>
+          <form onSubmit={handleSubmit(resetPasswordForEmail)}>
+            <div className={"input-container"}>
+              <label htmlFor="email" className="sr-only">
+                Enter your email
+              </label>
+              {errors.email && (
+                <ErrorMessage
+                  dataCy={"input-email-error"}
+                  message={"Please enter your email address"}
+                />
+              )}
+              <div className="input">
+                <input
+                  className={`bg-zinc-50 !p-2.5 ${errors.email && "error"}`}
+                  data-cy={"input-email"}
+                  type="email"
+                  placeholder="Email"
+                  {...register("email", { required: true })}
+                />
+                <span>
+                  <EnvelopeIcon />
+                </span>
+              </div>
+            </div>
+            <button
+              data-cy={"button-submit-email"}
+              type={"submit"}
+              className={"btn w-full rounded-lg p-2.5"}
+            >
+              Send link
+            </button>
+          </form>
+          <Link
+            href={"/sign-in"}
+            className={"text-center text-sm text-gray-400"}
+            data-cy={"back-to-sign-in"}
           >
-            Send link
-          </button>
-        </form>
-        <Link
-          href={"/sign-in"}
-          className={"text-center text-sm text-gray-400"}
-          data-cy={"back-to-sign-in"}
-        >
-          Back to login
-        </Link>
-      </div>
+            Back to login
+          </Link>
+        </div>
+      )}
     </div>
   )
 }
