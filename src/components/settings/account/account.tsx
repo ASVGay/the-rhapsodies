c3770809fd504bf8f745eae819fb7abf57ab6f93
Marcel Hettinga <hettingamarcel@gmail.com>
2023-06-05 13:52:29 +0200

    Add forms

--- src/components/settings/account/account.tsx
+++ src/components/settings/account/account.tsx
@@ -1,14 +1,16 @@
 import React from "react"
 import SettingsWrapper from "@/components/settings/settings-wrapper"
-import { UserCircleIcon } from "@heroicons/react/24/outline"
+import {UserCircleIcon} from "@heroicons/react/24/outline"
 import SignOut from "@/components/settings/account/sign-out"
+import ChangePassword from "@/components/settings/account/change-password";
 
 const AccountSettings = () => {
-  return (
-    <SettingsWrapper category={"Account"} icon={UserCircleIcon}>
-      <SignOut />
-    </SettingsWrapper>
-  )
+    return (
+        <SettingsWrapper category={"Account"} icon={UserCircleIcon}>
+            <SignOut/>
+            <ChangePassword/>
+        </SettingsWrapper>
+    )
 }
 
 export default AccountSettings
