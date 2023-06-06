33a157c8854192b6ef18d6b1d541f623d9e81493
Kevin Breurken <kevinbreurken@gmail.com>
2023-06-06 23:18:04 +0200

    Remove duplicate policies

--- supabase/migrations/20230606211508_song_instrument.sql
+++ supabase/migrations/20230606211508_song_instrument.sql
@@ -1,19 +1,3 @@
-create policy "Members are deletable by everyone."
-on "public"."member"
-as permissive
-for delete
-to authenticated, anon
-using (true);
-
-
-create policy "Members are viewable by everyone."
-on "public"."member"
-as permissive
-for select
-to authenticated, anon
-using (true);
-
-
 create policy "Enable "
 on "public"."song_instrument"
 as permissive
