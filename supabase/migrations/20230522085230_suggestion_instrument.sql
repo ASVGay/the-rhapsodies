3f27185393367c6ce63eb702e1346592e589daa2
Kevin Breurken <kevinbreurken@gmail.com>
2023-06-06 23:41:18 +0200

    Revert migration file

---
+++ supabase/migrations/20230522085230_suggestion_instrument.sql
@@ -0,0 +1,8 @@
+drop policy "Enable insert for authenticated users only" on "public"."suggestion_instrument";
+
+create policy "Enable insert for authenticated users only"
+on "public"."suggestion_instrument"
+as permissive
+for insert
+to authenticated
+with check (true);
