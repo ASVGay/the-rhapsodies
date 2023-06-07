4d1b9c854a67ab07a97c50e24e414a4b4779e70d
Kevin Breurken <kevinbreurken@gmail.com>
2023-06-08 01:25:00 +0200

    Update src/pages/suggestions/new/index.tsx
    Co-authored-by: Rens Doodeman <99396963+rensdoodeman@users.noreply.github.com>

--- src/pages/suggestions/new/index.tsx
+++ src/pages/suggestions/new/index.tsx
@@ -25,7 +25,7 @@ const NewSuggestion = () => {
   const user = useUser()
 
   const saveSuggestion = (onSuccess: () => void, onError: () => void) => {
-    if (user) {
+    if (!user) return
       insertSuggestion(supabase, suggestion, user.id)
         .then((response) => {
           if (response.error) {
