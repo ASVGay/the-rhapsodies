a95ae8b5be2a95c753f225a26d51b5d83e430d4d
Kevin Breurken <kevinbreurken@gmail.com>
2023-06-07 20:41:40 +0200

    Remove update from migration song_instrument

--- supabase/migrations/20230606211508_song_instrument.sql
+++ supabase/migrations/20230606211508_song_instrument.sql
@@ -1,10 +1,3 @@
-create policy "Enable update for all authenticated users"
-on "public"."song_instrument"
-as permissive
-for update
-to authenticated
-using (true);
-
 create policy "Enable delete for all authenticated users"
 on "public"."song_instrument"
 as permissive
