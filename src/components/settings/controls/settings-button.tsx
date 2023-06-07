959be6776badcb2a9a9434cb00dc249c96872c7d
F.S. Koulen <feryllia.koulen@hva.nl>
2023-06-07 02:04:28 +0200

    Replace Sign Out JSX with SettingsButton

--- src/components/settings/controls/settings-button.tsx
+++ src/components/settings/controls/settings-button.tsx
@@ -11,14 +11,14 @@ interface NotificationButtonProps {
 const SettingsButton = ({ dataCy, disabled, onClick, text }: NotificationButtonProps) => (
   <button
     type={"button"}
-    className={`flex w-full items-center justify-between hover:cursor-pointer disabled:cursor-not-allowed 
+    className={`group flex w-full items-center justify-between hover:cursor-pointer disabled:cursor-not-allowed 
     ${disabled && "pointer-events-none select-none opacity-20"}`}
     data-cy={dataCy}
     disabled={disabled}
     onClick={onClick}
   >
     <p>{text}</p>
-    <ChevronRightIcon height={16} width={16} className={"text-zinc-400"} />
+    <ChevronRightIcon height={16} width={16} className={"group-hover:text-moon-600"} />
   </button>
 )
 
