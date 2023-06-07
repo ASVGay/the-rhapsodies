c2080ed1fd142e5c98dc77f054d5b4489a3bb2d1
Kevin Breurken <kevinbreurken@gmail.com>
2023-06-07 21:54:30 +0200

    Add update to song_instrument policy

--- supabase/migrations/20230606211508_song_instrument.sql
+++ supabase/migrations/20230606211508_song_instrument.sql
@@ -4,3 +4,10 @@ as permissive
 for delete
 to authenticated
 using (true);
+
+create policy "Enable update for all authenticated users"
+on "public"."song_instrument"
+as permissive
+for update
+to authenticated
+using (true);
