5f1babf37cad1984cd3ea85464fe1ef710c7c742
F.S. Koulen <feryllia.koulen@hva.nl>
2023-06-12 18:17:39 +0200

    Import correct Database for handleNoUser

--- src/helpers/account-settings.ts
+++ src/helpers/account-settings.ts
@@ -2,13 +2,11 @@ import { toast } from "react-toastify"
 import { NextRouter } from "next/router"
 import { SupabaseClient } from "@supabase/supabase-js"
 import { UseFormSetError } from "react-hook-form"
+import { Database } from "@/types/database"
 
 const signOutError = () => toast.error("Something went wrong while logging out.")
 
-export const handleNoUser = <Database extends any>(
-  supabase: SupabaseClient<Database>,
-  router: NextRouter
-) => {
+export const handleNoUser = (supabase: SupabaseClient<Database>, router: NextRouter) => {
   toast.error("Something went wrong while retrieving your data. Please sign in again.")
   supabase.auth
     .signOut()
