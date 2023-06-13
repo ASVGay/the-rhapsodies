68c9dbfa277518fe6120b37c46736e4416fc46db
Marcel Hettinga <hettingamarcel@gmail.com>
2023-06-13 18:31:28 +0200

    Re-add type

--- supabase/migrations/20230612130312_add_events.sql
+++ supabase/migrations/20230612130312_add_events.sql
@@ -1,11 +1,12 @@
+    CREATE TYPE EVENT_TYPE AS ENUM ('Brainstormborrel', 'Rehearsal');
     CREATE TYPE ATTENDING AS ENUM ('present', 'absent', 'undecided');
 
 
     CREATE TABLE event (
       id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
       event_type EVENT_TYPE NOT NULL,
-      event_start_time TIMESTAMP WITH TIME ZONE NOT NULL,
-      event_end_time TIMESTAMP WITH TIME ZONE,
+      start_time TIMESTAMP WITH TIME ZONE NOT NULL,
+      end_time TIMESTAMP WITH TIME ZONE,
       location TEXT
     );
 
