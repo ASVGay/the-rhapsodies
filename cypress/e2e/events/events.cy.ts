7d5118021ae3e9de30b252154093405505a8ae2b
F.S. Koulen <feryllia.koulen@hva.nl>
2023-06-18 14:14:57 +0200

    Adjust test for get_events_with_attendance

--- cypress/e2e/events/events.cy.ts
+++ cypress/e2e/events/events.cy.ts
@@ -1,36 +1,37 @@
 import { interceptIndefinitely } from "../helpers/interception.helper"
 
 describe("events page", () => {
+  const eventsRequestPath = "/rest/v1/rpc/get_events_with_attendance"
+
   beforeEach(() => {
     cy.login()
-    cy.visit("events")
   })
 
   it("should display correct amount of events", () => {
-    cy.intercept("GET", "/rest/v1/event*", { fixture: "mock-events.json" }).as("mockedRequest")
-    cy.wait("@mockedRequest")
-    cy.wait(1000)
+    cy.intercept("GET", eventsRequestPath, { fixture: "mock-events.json" })
+    cy.visit("events")
     cy.data("event-list").children().should("have.length", 4)
   })
 
   it("should display loader", () => {
-    const interception = interceptIndefinitely("GET", "/rest/v1/event*")
+    const interception = interceptIndefinitely(eventsRequestPath)
+    cy.visit("events")
     cy.data("song-list-spinner").should("exist")
     interception.sendResponse()
     cy.data("song-list-spinner").should("not.exist")
   })
 
   it("should display correct error message when error is thrown", () => {
-    cy.intercept("GET", "/rest/v1/event*", {
+    cy.intercept(eventsRequestPath, {
       forceNetworkError: true,
     })
+    cy.visit("events")
     cy.data("no-events-text").contains("Failed to load events.")
   })
 
   it("should display correct error message when there are no events yet", () => {
-    cy.intercept("GET", "/rest/v1/event*", []).as("mockedRequest")
-    cy.wait("@mockedRequest")
-    cy.wait(1000)
+    cy.intercept(eventsRequestPath, [])
+    cy.visit("events")
     cy.data("no-events-text").contains("No events have been added yet.")
   })
 })
