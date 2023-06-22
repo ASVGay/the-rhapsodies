dc46a0dd6033aff71a5818fbc9e900621c97a055
F.S. Koulen <feryllia.koulen@hva.nl>
2023-06-22 02:38:13 +0200

    Use page-wrapper & page-header classes on home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import { ExclamationTriangleIcon } from "@heroicons/react/24/outline"
 import EventCard from "@/components/events/event-card"
 import { getEventsWithAttendees } from "@/services/event.service"
 import { getDisplayName } from "@/services/authentication.service"
+import { toast } from "react-toastify"
 
 export default function Home() {
   const [showSpinner, setShowSpinner] = useState<boolean>(true)
@@ -18,47 +19,60 @@ export default function Home() {
   const uid = useUser()?.id
 
   useEffect(() => {
-    setShowSpinner(true)
+    const fetchData = async () => {
+      setShowSpinner(true)
 
-    if (uid) {
-      getDisplayName(supabase, uid).then(({ data, error }) => {
+      if (uid) {
+        const { data, error } = await getDisplayName(supabase, uid)
         if (data) setDisplayName(data.display_name)
-      })
+        if (error) toast.error(`Something went wrong.`, { toastId: "name-error" })
+      }
+
+      const { data, error } = await getEventsWithAttendees(supabase)
+      if (data) setUpcomingEvent((data as EventWithAttendance[])[0])
+      if (error) setShowEventError(true)
+
+      setShowSpinner(false)
     }
 
-    getEventsWithAttendees(supabase)
-      .then(({ data, error }) => {
-        if (data) setUpcomingEvent((data as EventWithAttendance[])[0])
-        else setShowEventError(true)
-      })
-    setShowSpinner(false)
-  }, [])
+    fetchData().catch(() =>
+      toast.error("Something went wrong while retrieving data.", { toastId: "fetch-error" })
+    )
+  }, [supabase, uid])
 
-
-  return <>
-    {showSpinner ? (
-      <div className={"h-[75vh] text-center"}>
-        <Spinner size={10} />
-      </div>
-    ) : (<>
-        <div className={"m-4 flex flex-col pt-2 gap-4"}>
-          <div className={"flex flex-col w-full"}>
-            <b className={"text-2xl leading-8"}>Home</b>
-          </div>
-          <div className={"flex justify-center items-center flex-col text-center gap-4"}>
-            <p><span className={"text-moon-400"}>Hi {displayName}! </span>Check out the next event or start browsing the app.</p>
-            {upcomingEvent &&
-              <EventCard key={upcomingEvent.id} event={upcomingEvent} setShowSpinner={setShowSpinner} />
-            }
-            {showEventError &&
-              <div className={"max-w-m flex items-center justify-center gap-4 text-zinc-400"}>
-                <div><ExclamationTriangleIcon className={"h-[50px] w-[50px]"} /></div>
-                <p>No upcoming events..</p>
-              </div>
-            }
-          </div>
+  return (
+    <>
+      {showSpinner ? (
+        <div className={"h-[75vh] text-center"}>
+          <Spinner size={10} />
         </div>
-      </>
-    )}
-  </>
+      ) : (
+        <>
+          <div className={"page-wrapper"}>
+            <div className={"page-header"}>
+              Welcome{displayName && <span className={"text-moon-400"}> {displayName}</span>}!
+            </div>
+            <div className={"flex flex-col items-center justify-center gap-2 text-center"}>
+              <p>Check out the next event or start browsing the app.</p>
+              {upcomingEvent && (
+                <EventCard
+                  key={upcomingEvent.id}
+                  event={upcomingEvent}
+                  setShowSpinner={setShowSpinner}
+                />
+              )}
+              {showEventError && (
+                <div className={"max-w-m flex items-center justify-center gap-4 text-zinc-400"}>
+                  <div>
+                    <ExclamationTriangleIcon className={"h-[50px] w-[50px]"} />
+                  </div>
+                  <p>No upcoming events..</p>
+                </div>
+              )}
+            </div>
+          </div>
+        </>
+      )}
+    </>
+  )
 }
