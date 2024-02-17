3bf82b695892b1210396491122a2438c144a1a69
F.S. Koulen <feryll.koulen@hva.nl>
2024-02-17 13:04:24 +0100

    Add intercepts for DELETE, POST, and PATCH requests in edit-suggestion.cy.ts

--- cypress/e2e/edit-suggestion/edit-suggestion.cy.ts
+++ cypress/e2e/edit-suggestion/edit-suggestion.cy.ts
@@ -35,9 +35,10 @@ describe("when the user wants to edit a suggestion", () => {
         cy.visit(`suggestions/edit/${userSuggestion}`)
         // Wait so content can render properly and set up submit events
         cy.wait(500)
-        cy.intercept("DELETE", "/rest/v1/song_instrument*", { statusCode: 204 }).as(
-          "deleteInstrument",
-        )
+        // Set up intercepts
+        cy.intercept("DELETE", "/rest/v1/song_instrument*", { statusCode: 204 })
+        cy.intercept("POST", "/rest/v1/song_instrument*", { statusCode: 201 })
+        cy.intercept("PATCH", "/rest/v1/song_instrument*", { statusCode: 201 })
       })
 
       it("should maintain changes to the song information when revisiting ", () => {
@@ -98,8 +99,10 @@ describe("when the user wants to edit a suggestion", () => {
         cy.data("submit-suggestion-btn")
           .click()
           .then(() => {
-            cy.wait("@deleteInstrument")
-            cy.location("pathname").should("equal", `/suggestions/${userSuggestion}`)
+            cy.location("pathname", { timeout: 10000 }).should(
+              "equal",
+              `/suggestions/${userSuggestion}`,
+            )
           })
       })
     })
